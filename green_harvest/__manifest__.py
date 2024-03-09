b5c0f2dfb805fa7b10b0ea4d57e15d3c5626794d
dariomolinavaldes <dariodurcal@gmail.com>
2024-03-09 10:59:22 +0100

    manifest

--- green_harvest/__manifest__.py
+++ green_harvest/__manifest__.py
@@ -2,21 +2,28 @@
 {
     'name': "GreenHarvest",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """   
+    Bienvenido a GreenHarvestERP, tu solución integral para la gestión agrícola y la venta de productos orgánicos. 
+    Optimiza tus operaciones y promueve prácticas agrícolas sostenibles con nuestra plataforma. 
+    Desde la producción hasta la comercialización, GreenHarvestERP te ofrece todas las herramientas que necesitas para 
+    cultivar con responsabilidad y llevar tus productos al mercado con éxito.
+       """,
+
 
     'description': """
-        Long description of module's purpose
+         GreenHarvestERP es la solución definitiva para la gestión eficiente de tu empresa agrícola. 
+        Desde el control de inventario hasta la contabilidad, nuestro ERP está diseñado para satisfacer 
+        todas las necesidades de tu negocio. Descubre una nueva forma de cultivar y vender productos orgánicos 
+        con GreenHarvestERP.
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Dario Molina Valdes",
+    'website': "https://www.youtube.com/watch?v=5yCsjASSd1M",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Tools',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
